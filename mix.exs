36729ef0d09acea164976a59d66ca3b67539a8f0
Sergio Arbeo <sergio.arbeo@dockyard.com>
2024-05-16 13:12:12 +0200

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule LiveViewEvents.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/DockYard/live_view_events"
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [
