d233a6b77db3147da5695e665b0642de97c7756b
Sergio Arbeo <sergio.arbeo@dockyard.com>
2024-05-16 12:47:39 +0200

    Normalize params

--- test/live_view_events/notify_test.exs
+++ test/live_view_events/notify_test.exs
@@ -10,4 +10,11 @@ defmodule LiveViewEvents.NotifyTest do
   test "notify_to/3 with `nil` target does not break" do
     Notify.notify_to(nil, "event", %{some: :params})
   end
+
+  test "notify_to/2 add default empty params" do
+    Notify.notify_to(:self, "event")
+
+    assert_receive {"event", %{} = params}
+    assert Enum.empty?(params)
+  end
 end
